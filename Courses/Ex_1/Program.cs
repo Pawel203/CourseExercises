107d04aa2f7dfc7468b971e3b5fa661d889b1057
Paweł <Pawe@DESKTOP-A3GQSBJ>
2025-05-07 20:16:17 +0200

    Opracowanie lekcji 2 (stałe i zmienne)

--- Courses/Ex_1/Program.cs
+++ Courses/Ex_1/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_2 //przestrzennazw
+{
+    internal class Program //klasa
+    {
+        static void Main(string[] args) //metoda
+        {
+            var zmienna = "Hello";
+
+            //całkowite
+            sbyte zmienna1 = sbyte.MaxValue;
+            byte zmienna2 = byte.MaxValue;
+            ushort zmienna3 = ushort.MaxValue;
+            short zmienna4 = short.MaxValue;
+            int zmienna5 = int.MaxValue;
+            uint zmienna6 = uint.MaxValue;
+            long zmienna7 = long.MaxValue;
+            ulong zmienna8 = ulong.MaxValue;
+
+            //przecinkowe
+            float zmienna9 = 1.431F;
+            double zmienna10 = 1.823;
+            decimal zmienna11 = 2.738M;
+
+            //text
+            string zmienna12 = "Hello";
+            char zmienna13 = '$';
+
+            //logiczny
+            bool zmienna14 = false;
+            bool zmienna15 = true;
+
+            //stala
+            const float PI = 3.14F;
+
+            Console.ReadKey();
+        }
+    }
+}
