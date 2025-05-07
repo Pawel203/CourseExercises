e2742b89e232de1883a9b2d3536db2a190be068a
Paweł <Pawe@DESKTOP-A3GQSBJ>
2025-05-07 21:07:24 +0200

    Opracowanie lekcji 3 (Operatory arytmetyczne i logiczne)

--- Courses/Ex_2/Program.cs
+++ Courses/Ex_2/Program.cs
@@ -10,6 +10,33 @@ namespace Ex_2
     {
         static void Main(string[] args)
         {
+            string zmienna; //deklaracja zmiennej
+            zmienna = "Hello"; //przypisanie do wczesniej zdeklarowanej zmiennej
+
+            int a = 2;
+            int b = 3;
+            int c = 4;
+
+            int dodawanie = a + b;
+            int odejmowanie = a - b;
+            int mnozenie = a * b;
+            float dzielenie = a / b;
+            int modulo = a % b;
+
+            Math.Pow(2, 3);
+            Math.Abs(-7);
+
+            c++;
+            c--;
+
+            bool d = !(2 == 2); //negacja
+
+            //&& - AND (sprawdza sie przed OR-em)
+            //|| - OR
+
+            Console.WriteLine(c);
+            Console.ReadKey();
+
         }
     }
 }
