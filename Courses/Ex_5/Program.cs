31bf69b80505cdae5b08147efa034082c724c3ac
Paweł <Pawe@DESKTOP-A3GQSBJ>
2025-05-08 20:06:17 +0200

    Opracowanie lekcji 6 (Konwersja typów, rzutowanie, komentarze i inne)

---
+++ Courses/Ex_5/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //ctrl + k + d - ukladanie kodu
+            //ctrl + k + c - komentowanie
+            //ctrl + k + u - odkomentowanie
+
+            //if (Console.ReadKey().Key == ConsoleKey.D1)
+            //{
+            //    Console.WriteLine("prawda");
+            //}
+            //Console.ReadLine();
+
+            //string s = Console.ReadLine();
+
+            //int i = int.Parse(s) + 2;
+            //Console.WriteLine(i);
+            //s = i.ToString();
+            //Console.WriteLine(s+"lala");
+
+            string s1 = "Witaj";
+            string s2 = "Świecie";
+
+            Console.WriteLine(s1 + " " + s2 + " {0} {1}", s1,s1);
+
+            //Console.BackgroundColor = ConsoleColor.Green;
+            //Console.BackgroundColor = ConsoleColor.Red;
+            //Console.ResetColor();
+            //Console.Clear();
+
+            Console.ReadKey();
+        }
+    }
+}
