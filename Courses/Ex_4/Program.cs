f347a99b55a62384bc570f0dbcd7ef0386c5b4d9
Paweł <Pawe@DESKTOP-A3GQSBJ>
2025-05-08 18:17:59 +0200

    Opracowanie lekcji 5 (Pętle while, do while oraz for)

---
+++ Courses/Ex_4/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //WHILE
+            int a = 0;
+            while (a < 5)
+            {
+                Console.WriteLine(a);
+                a++;
+            }
+
+            Console.WriteLine();
+
+            //FOR
+            int i = 0;
+            for (; i < 5; i++) 
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine(i);
+
+            //DO WHILE
+
+            do
+            {
+                Console.WriteLine("hej");
+                i++;
+            }
+            while (i < 10);
+        }
+    }
+}
+
