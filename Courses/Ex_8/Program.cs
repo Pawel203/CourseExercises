b061cae68c7f21531ecf248dea1e62504d2f28a6
Paweł <Pawe@DESKTOP-A3GQSBJ>
2025-05-09 21:06:32 +0200

    Opracowanie lekcji 9 (Instrukcje Switch)

---
+++ Courses/Ex_8/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int liczba;
+            liczba = int.Parse(Console.ReadLine());
+
+            switch (liczba)
+            {
+                case 1:
+                    Console.WriteLine("jeden");
+                    break;
+                case 2:
+                    Console.WriteLine("dwa");
+                    break;
+                case 3:
+                    Console.WriteLine("trzy");
+                    break;
+                default:
+                    Console.WriteLine("inna wartosc");
+                    break;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
