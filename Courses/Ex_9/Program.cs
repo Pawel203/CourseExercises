81e4af545b352a5644caf1ef6e6c22f0adc233cd
Paweł <Pawe@DESKTOP-A3GQSBJ>
2025-05-09 22:47:30 +0200

    Opracowanie lekcji 10 (Tablice cz.1)

---
+++ Courses/Ex_9/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] tablica = new int[365];
+            string[] dniTygodnia = {"poniedzialek", "wtorek", "sroda", "czwartek", "piatek", "soobota", "niedziela"};
+
+            tablica[0] = 22;
+
+            for (int i = 0; i < dniTygodnia.Length; i++)
+            {
+                Console.WriteLine(dniTygodnia[i]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
