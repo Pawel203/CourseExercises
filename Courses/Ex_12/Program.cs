cacbe36a9d8f2f5b945315ed0b49bbb35aaadf79
Paweł <Pawe@DESKTOP-A3GQSBJ>
2025-05-10 16:39:27 +0200

    Opracowanie lekcji 13 (Funkcje cz.2)

---
+++ Courses/Ex_12/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Dodaj(2, 4)); 
+            Console.ReadKey();
+        }
+
+        static int Dodaj(int a, int b)
+        {
+            return (a + b);
+        }
+    }
+}
