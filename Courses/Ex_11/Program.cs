fced23913dba6d2512859001a51e20d1b7c1dcd7
Paweł <Pawe@DESKTOP-A3GQSBJ>
2025-05-10 16:29:19 +0200

    Opracowanie lekcji 12 (Funkcje cz.1)

---
+++ Courses/Ex_11/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Dodaj(3, 6, d:2);
+
+            Console.ReadKey();
+        }
+
+        static void Dodaj(int a, int b)
+        {
+            int wynik = a + b;
+            Console.WriteLine("Wynik dodawania: " + wynik);
+        }
+        static void Dodaj(int a, int b, int c = 0) //podajac 3 argumenty c zostanie zamienione
+        {
+            int wynik = a + b + c;
+            Console.WriteLine("Wynik dodawania: " + wynik);
+        }
+        static void Dodaj(int a, int b, int c = 2, int d = 0) //podajac 3 argumenty c zostanie zamienione
+        {
+            int wynik = a + b + c + d;
+            Console.WriteLine("Wynik dodawania: " + wynik);
+        }
+    }
+}
+
+//void - nie zwraca wyniku, wynik jest tylko w srodku
