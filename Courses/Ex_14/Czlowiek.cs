35204ef4ee4ff08903f6f41816f1dfa268f8cb15
Paweł <Pawe@DESKTOP-A3GQSBJ>
2025-05-11 12:41:32 +0200

    Opracowanie lekcji 15 (Klasy i obiekty cz.1)

---
+++ Courses/Ex_14/Czlowiek.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_14
+{
+    class Czlowiek
+    {
+        public string imie = "Paweł";
+
+        public void PrzedstawSie()
+        {
+            Console.WriteLine("Mam na imię " + imie);
+        }
+
+    }
+}
---
+++ Courses/Ex_14/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Czlowiek adi = new Czlowiek();
+            Console.WriteLine(adi.imie);
+            adi.imie = "Bartek";
+            Console.WriteLine(adi.imie);
+
+            Czlowiek adi2 = new Czlowiek();
+            adi2.imie = "Adrian";
+            Console.WriteLine(adi2.imie);
+
+            adi.PrzedstawSie();
+            adi2.PrzedstawSie();
+
+            Console.ReadKey();
+        }
+    }
+}
