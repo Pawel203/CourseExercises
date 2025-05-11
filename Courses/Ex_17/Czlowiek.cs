8d80d91de9743fc64e05a70f0363d0e1b0db3cf1
Paweł <Pawe@DESKTOP-A3GQSBJ>
2025-05-11 20:04:44 +0200

    Opracowanie lekcji 18 (Właściwości)

---
+++ Courses/Ex_17/Czlowiek.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_17
+{
+    internal class Czlowiek
+    {
+        public string imie;
+
+        private int wiek;
+
+        //public int getWiek()
+        //{
+        //    return wiek;
+        //}
+
+        //public void setWiek(int w)
+        //{
+        //    if (w >= 0 && w <= 150)
+        //        wiek = w;
+        //}
+
+        //public int wiek { get; set; }
+
+        public int Wiek
+        {
+            get { return wiek; }
+            set
+            {
+                if (value >= 0 && value <= 200)
+                {
+                    wiek = value;
+                }
+                else 
+                {
+                    wiek = 0;
+                }
+            }
+        }
+
+    }
+}
---
+++ Courses/Ex_17/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Czlowiek czlowiek1 = new Czlowiek();
+            czlowiek1.imie = "Paweł";
+            //czlowiek1.setWiek(-20
+            //czlowiek1.wiek = 20;
+            czlowiek1.Wiek = 20;
+
+
+            Console.WriteLine($"Jestem {czlowiek1.imie} lat {czlowiek1.Wiek}");
+
+
+            Console.ReadKey();
+        }
+    }
+}
