9d27e05beeb121c6546eef55b16be4d1b8b19790
Paweł <Pawe@DESKTOP-A3GQSBJ>
2025-05-11 13:10:31 +0200

    Opracowanie lekcji 16 (Klasy i obiekty cz.2 )[Konstruktory i destruktory]

---
+++ Courses/Ex_15/Czlowiek.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_15
+{
+    class Czlowiek
+    {
+
+        public Czlowiek()
+        {
+        }
+
+        public Czlowiek(string imieKonstruktora)
+        {
+            imie = imieKonstruktora;
+        }
+
+        public Czlowiek(string imieKonstruktora, string nazwiskoKonstruktora)
+        {
+            imie = imieKonstruktora;
+            nazwisko = nazwiskoKonstruktora;
+        }
+        public string nazwisko = "Owczarzak";
+        public string imie = "Paweł";
+
+        public void PrzedstawSie()
+        {
+            Console.WriteLine("Mam na imię " + imie + " " + nazwisko);
+        }
+
+        ~Czlowiek()
+        {
+            Console.WriteLine("Jestem destruktorem");
+        }
+    }
+}
---
+++ Courses/Ex_15/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Czlowiek adi = new Czlowiek("Dawid");
+            adi.PrzedstawSie();
+            adi.imie = "Bartek";
+            adi.PrzedstawSie();
+
+
+            Console.ReadKey();
+        }
+    }
+}
