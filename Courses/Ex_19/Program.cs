9e5f3cbd08a373980674d7506754e69d86a3a44f
Paweł <Pawe@DESKTOP-A3GQSBJ>
2025-05-12 15:43:10 +0200

    Opracowanie lekcji 20 (Operacje na Stringach)

---
+++ Courses/Ex_19/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_19
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            int ile = 100;
+            stopwatch.Start();
+            BudujString(ile);
+            stopwatch.Stop();
+            Console.WriteLine(stopwatch.ElapsedTicks);
+
+            Console.WriteLine();
+
+            stopwatch.Reset();
+            stopwatch.Start();
+            BudujStringBuilder(ile);
+            stopwatch.Stop();
+            Console.WriteLine(stopwatch.ElapsedTicks);
+
+        }
+        /// <summary>
+        /// Metoda buduje string
+        /// </summary>
+        /// <param name="ile">Podaj dlugosc petli</param>
+        /// <returns>Zwracamy gotowy tekst</returns>
+
+        public static string BudujString(int ile)
+        {
+            string tekst = "";
+            for (int i = 0; i < ile; i++)
+            {
+                tekst += i;
+            }
+            return tekst;
+        }
+
+        public static string BudujStringBuilder(int ile) 
+        {
+            string tekst = "";
+            StringBuilder stringBuilder = new StringBuilder();
+            for (int i = 0; i < ile; i++)
+            { 
+                stringBuilder.Append(i);
+            }
+            return stringBuilder.ToString();
+        }
+    }
+}
