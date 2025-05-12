45327f5e33314325e0536376bc9441e03d7c8171
Paweł <Pawe@DESKTOP-A3GQSBJ>
2025-05-12 17:44:34 +0200

    Opracowanie lekcji 24 (Kolekcje - Dictionary)

---
+++ Courses/Ex_23/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_23
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> dictionary = new Dictionary<int, string>();
+
+            dictionary.Add(1, "Ania");
+            dictionary.Add(2, "Bartek");
+
+
+            foreach (var item in dictionary)
+            {
+                Console.WriteLine("{0} : {1}", item.Key, item.Value);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
