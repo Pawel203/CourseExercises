a8fa1345fc27cbaaf0092f3681102b823486b9a8
Paweł <Pawe@DESKTOP-A3GQSBJ>
2025-05-12 16:46:13 +0200

    Opracowanie lekcji 21 (CultureInfo)

---
+++ Courses/Ex_20/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Ex_20
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var c = CultureInfo.GetCultures(CultureTypes.AllCultures);
+
+            foreach (var item in c)
+            {
+                Console.WriteLine(item.Name);
+                Console.WriteLine(item.NumberFormat.NumberDecimalSeparator);
+            }
+
+            int a = -5;
+            float b = 3.14f;
+
+            Console.WriteLine($"{a}\n{b}");
+
+            Console.ReadKey();
+        }
+    }
+}
