728984538dfccda6a1a9cffe09ff81f7db277aca
Paweł <Pawe@DESKTOP-A3GQSBJ>
2025-05-20 15:57:36 +0200

    Opracowanie lekcji 27 (Dziedziczenie)

---
+++ Courses/Ex_26/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_26
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Ziemia ziemia = new Ziemia();
+            ziemia.Nazwa = "Ziemia";
+            Console.WriteLine(ziemia.Nazwa);
+
+            Console.ReadKey();
+        }
+    }
+
+    abstract class CialoNiebieskie
+    {
+        public string Nazwa { get; set; }
+        public void RuchObrotowy() 
+        {
+            Console.WriteLine("Kazde cialo niebieski posiada ruch obrotowy");
+        }
+    }
+    abstract class Planeta : CialoNiebieskie
+    {
+        void PoraDniaINocy()
+        {
+            Console.WriteLine("Pory Dnia i Nocy");
+        }
+    }
+
+    class Ziemia : Planeta
+    {
+        void Atmosfera()
+        {
+            Console.WriteLine("Atmosfera Ziemska");
+        }
+    }
+}
