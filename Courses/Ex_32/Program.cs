ff7cc96088220f731ab36a9362096dd182c4c05c
Paweł <Pawe@DESKTOP-A3GQSBJ>
2025-05-22 19:09:17 +0200

    Opracowanie lekcji 33 (Wyjątki - Try Catch)

---
+++ Courses/Ex_32/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_32
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine(Dzielenie(0));
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine("Dzielisz przez zero " + e);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("O nie cos poszlo nie tak! " + e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Ja i tak posprzataem");
+            }
+
+
+            Console.ReadKey();
+        }
+
+        public static float Dzielenie(int a)
+        {
+            if (a == 5)
+            {
+                throw new Exception("Nigdy nie dziel przez 5");
+            }
+            else
+            {
+                return 10 / a;
+            }
+        }
+    }
+}
