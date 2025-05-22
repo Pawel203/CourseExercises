6abfbabd7eddbfe69d1278617ceb9d78432e1c5d
Paweł <Pawe@DESKTOP-A3GQSBJ>
2025-05-22 18:03:42 +0200

    Opracowanie lekcji 32 (Enum)

---
+++ Courses/Ex_31/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_31
+{
+    enum PoryRoku
+    {
+        Wiosna = 15, Lato= 25, Jesień= 5, Zima = -5
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PoryRoku poraRoku = PoryRoku.Zima;
+            Console.WriteLine((int)poraRoku);
+        }
+    }
+}
