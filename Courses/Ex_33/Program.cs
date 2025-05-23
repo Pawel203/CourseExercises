1caba34ad6c012be32f4a876bac7f345e65c81d0
Paweł <Pawe@DESKTOP-A3GQSBJ>
2025-05-23 23:53:43 +0200

    Opracowanie lekcji 34 (Wyjątki cz. 2)

---
+++ Courses/Ex_33/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_33
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine(Dzielenie(5));
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine("Dzielisz przez zero " + e);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+
+            Console.ReadKey();
+        }
+
+        public static float Dzielenie(int a)
+        {
+            if (a == 5)
+            {
+                throw new DivadeByFiveException("Nie dziel przez 5!");
+            }
+            else
+            {
+                return 10 / a;
+            }
+        }
+    }
+
+    class DivadeByFiveException : Exception
+    {
+        public DivadeByFiveException()
+        {
+            Console.WriteLine("Bład");
+        }
+
+        public DivadeByFiveException(string message) : base(message + "Dzielenie przez 5!")
+        {
+        }
+    }
+}
