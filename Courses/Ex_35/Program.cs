846d8d4ee7651fec9eb6ae8006bf3096bf2a3cd4
Paweł <Pawe@DESKTOP-A3GQSBJ>
2025-05-24 21:58:49 +0200

    Opracowanie lekcji 36 (operacje na Plikach)

---
+++ Courses/Ex_35/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_35
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\Paweł\Desktop";
+            StreamWriter sw;
+            if(!File.Exists(path))
+            {
+                sw = File.CreateText(path);
+                Console.WriteLine("Plik zostal utworzony");
+            }
+            else
+            {
+                sw = new StreamWriter(path, true);
+                Console.WriteLine("Plik zostal otwarty");
+            }
+            Console.WriteLine("Podaj teskt: ");
+            string tekst = Console.ReadLine();
+
+            sw.WriteLine(tekst);
+            sw.Close();
+
+            StreamReader sr = File.OpenText(path);
+            string s = "";
+            int i = 1;
+            Console.WriteLine("\nZAWARTOSC PLIKU:");
+            while ((s = sr.ReadLine()) != null)
+            {
+                Console.WriteLine(i++ + ". " + s);
+            }
+            sr.Close();
+        }
+    }
+}
