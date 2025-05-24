e50454ab5adb2a7186a8e561e212529194b16222
Paweł <Pawe@DESKTOP-A3GQSBJ>
2025-05-24 17:56:02 +0200

    Opracowanie lekcji 35 (is i as)

---
+++ Courses/Ex_34/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_34
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Zwierze zwierze = new Zwierze();
+            Kot kot = new Kot();
+            zwierze = new Kot();
+
+            Console.WriteLine(kot is Kot);
+
+            Kot kot2 = zwierze as Kot;
+
+            if (!(kot2 == null))
+            {
+                Console.WriteLine("Powiodlo sie");
+            }
+            else
+            {
+                Console.WriteLine("Nie powiodlo sie");
+            }
+        }
+    }
+    class Zwierze
+    {
+        public void DajGlos()
+        {
+            Console.WriteLine("Domyslny glos");
+        }
+    }
+    class Kot : Zwierze
+    {
+        public void PijMleko()
+        {
+            Console.WriteLine("Piję mleko");
+        }
+    }
+}
